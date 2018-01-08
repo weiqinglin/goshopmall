54c516cdaff833bf27b2466ceb9c8f806acf1cb6
weiqinglin <544489440@qq.com>
2018-01-08 18:35:24 +0800

    回复文本信息

--- Application/Home/Controller/WxController.class.php
+++ Application/Home/Controller/WxController.class.php
@@ -13,6 +13,14 @@ use Think\Exception;
 
 class WxController extends Controller{
 
+    private $_msgTpl = array(
+        'text'=>'<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content></xml>',
+        'image'=>'<xml><ToUserName>< ![CDATA[%s] ]></ToUserName><FromUserName>< ![CDATA[%s] ]></FromUserName><CreateTime>%s</CreateTime><MsgType>< ![CDATA[image] ]></MsgType><Image><MediaId>< ![CDATA[%s] ]></MediaId></Image></xml>',
+        'voice'=>'<xml><ToUserName>< ![CDATA[%s] ]></ToUserName><FromUserName>< ![CDATA[%s] ]></FromUserName><CreateTime>%s</CreateTime><MsgType>< ![CDATA[voice] ]></MsgType><Voice><MediaId>< ![CDATA[%s] ]></MediaId></Voice></xml>',
+        'video'=>'<xml><ToUserName>< ![CDATA[%s] ]></ToUserName><FromUserName>< ![CDATA[%s] ]></FromUserName><CreateTime>%s</CreateTime><MsgType>< ![CDATA[video] ]></MsgType><Video><MediaId>< ![CDATA[%s] ]></MediaId><Title>< ![CDATA[%s] ]></Title><Description>< ![CDATA[%s] ]></Description></Video> </xml>',
+        'music'=>'<xml><ToUserName>< ![CDATA[%s] ]></ToUserName><FromUserName>< ![CDATA[%s] ]></FromUserName><CreateTime>%s</CreateTime><MsgType>< ![CDATA[music] ]></MsgType><Music><Title>< ![CDATA[%s] ]></Title><Description>< ![CDATA[%s] ]></Description><MusicUrl>< ![CDATA[%s] ]></MusicUrl><HQMusicUrl>< ![CDATA[%s] ]></HQMusicUrl><ThumbMediaId>< ![CDATA[%s] ]></ThumbMediaId></Music></xml>',
+    );
+
     public function checkSignature(){
         $signature = $_GET['signature'];
         $timestamp = $_GET['timestamp'];
@@ -24,8 +32,54 @@ class WxController extends Controller{
         $tmp = sha1(implode('',$tmp));
         if($tmp == $signature && $echostr){
             echo $echostr;
+        }else{
+            $this->responseMsg();
         }
 
+    }
+
+    public function responseMsg(){
+        $postStr  = $GLOBALS["HTTP_RAW_POST_DATA"] == '' ? file_get_contents('php://input') : $GLOBALS["HTTP_RAW_POST_DATA"];
+        $postStr = simplexml_load_string($postStr,SimpleXMLElement,LIBXML_NOCDATA);
+        $this->_responseMsg($postStr);
+    }
+
+    protected function _responseMsg($obj){
+        switch (C('responseType')){
+            case 'image':
+                $this->reponseImage($obj);
+                break;
+            case 'voice':
+                $this->responseVoice($obj);
+                break;
+            case 'video':
+                $this->responseVedio($obj);
+                break;
+            case 'music':
+                $this->responseMusic($obj);
+                break;
+            default:
+                $this->responseText($obj);
+        }
+    }
+
+    public function reponseImage($obj){
+
+    }
+
+    public function responseVoice($obj){
+
+    }
+    public function responseVedio($obj){
+
+    }
+    public function responseMusic($obj){
+
+    }
+    public function responseText($obj){
+        $content = '你还好?';
+        $msg = sprintf($this->_msgTpl['text'],$obj->FromUserName,$obj->ToUserName,time(),$content);
+        echo $msg;
     }
 
     private function __getAccessToken(){
