e9a5d5d01e277378a9538ffb0372e7868b501c17
weiqinglin <544489440@qq.com>
2018-01-09 15:08:42 +0800

    回复图片

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -4,5 +4,5 @@ return array(
     'appID'=>'wxd9731fb846af4e7f',
     'appsecret'=>'ff935297ee911e783625b001c6fd2be4',
     'DEFAULT_THEME'=>'index',
-    'responseType'=>'scan'
+    'responseType'=>'image'
 );
